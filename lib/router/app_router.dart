56c618748bf497d4cd192766ae8c9082158bb730
Dev Eslam_mansour <101331596+Eslam0mansour@users.noreply.github.com>
2022-09-21 17:26:33 +0200

    eslam task login & signup

--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -1,11 +1,13 @@
 import 'package:algoriza_team_6_realestate_app/constants/screens.dart'
     as screens;
 import 'package:algoriza_team_6_realestate_app/screens/app_layout/app_layout.dart';
+import 'package:algoriza_team_6_realestate_app/screens/auth_pages/sign_up_screen.dart';
 import 'package:algoriza_team_6_realestate_app/screens/filter/filter_sceen.dart';
 import 'package:algoriza_team_6_realestate_app/screens/home/home_screen.dart';
 import 'package:algoriza_team_6_realestate_app/screens/setting/setting_screen.dart';
 import 'package:flutter/material.dart';
 
+import '../screens/auth_pages/login_screen.dart';
 import '../screens/onbording/onbording.dart';
 import '../screens/splash/splash_screen.dart';
 
@@ -21,7 +23,11 @@ class AppRouter {
       case screens.settingScreenRoute:
         return MaterialPageRoute(builder: (_) => SettingScreen());
         case screens.onboardingRoute:
-        return MaterialPageRoute(builder: (_) => Onboarding());
+        return MaterialPageRoute(builder: (_) => const Onboarding());
+        case screens.loginRoute:
+        return MaterialPageRoute(builder: (_) => Login());
+        case screens.signupRoute:
+        return MaterialPageRoute(builder: (_) => SignUp());
       case screens.appLayoutRoute:
         int? route = settings.arguments as int?;
         return MaterialPageRoute(
